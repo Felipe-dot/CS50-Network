1f9e47c19492a1856d82c7c793d9b64ba1064389
Felipe-dot <felipeluis610@gmail.com>
2026-03-01 10:03:03 -0300

    ✨ Create news path point to views function created

--- network/urls.py
+++ network/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("post", views.create_post, name="create_post"),
+    path("profile/<str:username>", views.profile, name="profile"),
+    path("following", views.following, name="following"),
+    path("post/<int:post_id>/edit", views.edit_post, name="edit_post"),
+    path("post/<int:post_id>/like", views.toggle_like, name="toggle_like")
 ]
